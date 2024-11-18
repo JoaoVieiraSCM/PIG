11d6530cb0b5ba310d2915f9631e51b2e4c66f42
joão vitor vieira <jvvscm@gmail.com>
2024-11-18 10:35:05 -0300

    Java Project

---
+++ src/test/java/com/db/sitebd/SitebdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.sitebd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitebdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
